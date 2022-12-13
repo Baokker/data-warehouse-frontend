c625f28d0f35565ba1228f8df96fd58a14d4d5d1
Baokker <1048098862@qq.com>
2022-12-13 20:28:24 +0800

    rearrange code

--- src/views/relation_search/index.vue
+++ src/views/relation_search/index.vue
@@ -8,37 +8,41 @@
       :visible.sync="dialogVisible"
       width="25%"
       center
-      @close="dialogVisible=false;"
+      @close="dialogVisible = false"
     >
       <div>
         <p>asin:{{ dialogData.asin }}</p>
-        <p>电影名:{{dialogData.name}}</p>
-        <p v-if="dialogData.director.length!==0" >
+        <p>电影名:{{ dialogData.name }}</p>
+        <p v-if="dialogData.director.length !== 0">
           导演：
           <span v-for="i in dialogData.director">{{ i }}, </span>
         </p>
-        <p v-if="dialogData.actor.length!==0">
+        <p v-if="dialogData.actor.length !== 0">
           演员：
           <span v-for="i in dialogData.actor">{{ i }}, </span>
         </p>
-        <p>评分:{{dialogData.score}}</p>
-        <p>评论总数:{{dialogData.commentNum}}</p>
+        <p>评分:{{ dialogData.score }}</p>
+        <p>评论总数:{{ dialogData.commentNum }}</p>
       </div>
       <div slot="footer">
-        <el-button type="primary" @click="viewOriginWeb()">查看原始网页</el-button>
-        <br><br>
+        <el-button type="primary" @click="viewOriginWeb()"
+          >查看原始网页</el-button
+        >
+        <br /><br />
         <el-button @click="dialogVisible = false">关闭</el-button>
       </div>
     </el-dialog>
 
-    <el-row style="height: 50vh;">
+    <el-row style="height: 50vh">
       <el-col :span="12">
         <el-form>
           <el-form-item>
             <el-button round @click="button">合作次数最多的演员</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button round @click="mostCooperateActorAndDirectorButton">合作次数最多的演员和导演</el-button>
+            <el-button round @click="mostCooperateActorAndDirectorButton"
+              >合作次数最多的演员和导演</el-button
+            >
           </el-form-item>
         </el-form>
       </el-col>
@@ -57,48 +61,47 @@
 </template>
 
 <script>
-const BASE_URL = 'https://81.68.102.171:5000/api'
+const BASE_URL = "https://81.68.102.171:5000/api";
 
-import axios from "axios"
+import axios from "axios";
 export default {
-
+  name: "RelationSearch",
   data() {
     return {
       dialogVisible: false,
       dialogData: {
-        asin: '1234',
-        name: '11',
-        director: ['小明', '小红', '小黑', '小白'],
-        actor: ['小蓝', '小黄', '小黑', '小白'],
+        asin: "1234",
+        name: "11",
+        director: ["小明", "小红", "小黑", "小白"],
+        actor: ["小蓝", "小黄", "小黑", "小白"],
         score: 2.5,
-        commentNum: 100
-      }
-    }
+        commentNum: 100,
+      },
+    };
   },
 
-  created(){
+  created() {
     console.log("test");
   },
 
-  methods:{
-      viewOriginWeb() {
-        //跳转网页
-      window.open("https:www.amazon.com/dp/"+this.dialogData.asin);
-      },
-    button(){
-      console.log("111")
+  methods: {
+    viewOriginWeb() {
+      //跳转网页
+      window.open("https:www.amazon.com/dp/" + this.dialogData.asin);
+    },
+    button() {
+      console.log("111");
       axios({
-            method: "get",
-            url: 'https://81.68.102.171:5000/ap/cooperation/with_actor/director',
-            params:{
-              director:"Tom McGrath",
-              time:6,
-            }
-          }).then(response=>{
-            console.log(res);
-          })
-    }
-  }
-}
+        method: "get",
+        url: "https://81.68.102.171:5000/ap/cooperation/with_actor/director",
+        params: {
+          director: "Tom McGrath",
+          time: 6,
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
 </script>
-
