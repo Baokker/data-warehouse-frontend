b9ba3b029549ab8f0e1ca6928dcc97d306ab2398
Baokker <1048098862@qq.com>
2022-12-13 18:54:05 +0800

    add self-defined axios

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || '数据仓库项目'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
--- src/views/test.vue
+++ src/views/test.vue
@@ -7,40 +7,11 @@ export default {
   // eslint-disable-next-line vue/name-property-casing
   name: "test",
   data() {
-    return {
-      BASE_URL: "http://81.68.102.171:5000/api/",
-    };
+    return {};
   },
   methods: {
     test() {
-      var axios = require("axios");
-
-      var config = {
-        method: "get",
-        url: this.BASE_URL + "/bytime/year",
-        params: {
-          year: 2008,
-        },
-      };
-
-      axios.interceptors.request.use((request) => {
-        console.log(request);
-        return request;
-      });
-
-      axios.interceptors.response.use((response) => {
-        console.log(response);
-        return response;
-      });
-
-      axios(config)
-        .then((response) => {
-          console.log(response.data);
-        })
-        // eslint-disable-next-line no-unused-vars
-        .catch(function (error) {
-          this.$message.error("当前网络异常，请稍后再试");
-        });
+      this.$axios.get("/bytime/year", { params: { year: 2008 } });
     },
   },
 };
